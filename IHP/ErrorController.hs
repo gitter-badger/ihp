8937e6434ba92a137f785fc1d2b19ea27ca08ad1
laobiz <pothjonas@gmail.com>
2020-05-25 08:34:58 +0200

    work

--- IHP/ErrorController.hs
+++ IHP/ErrorController.hs
@@ -32,8 +32,8 @@ handleNoResponseReturned controller = do
 handleGenericException :: (Show controller, ?requestContext :: RequestContext) => Exception.SomeException -> controller -> IO ResponseReceived
 handleGenericException exception controller = do
     let errorMessage = case exception of
-        _ : "ConversionFailed" : _ -> ("There is a database mismatch between your live database and your types. \n\nYou can fix this by either running 'make db' or by pressing 'Push to DB' in the Schema Designer.") 
-        _ -> ("An exception was raised while running the action " <> tshow controller <> ".\n\n")
+        _ : "ConversionFailed" : _  <- ("There is a database mismatch between your live database and your types. \n\nYou can fix this by either running 'make db' or by pressing 'Push to DB' in the Schema Designer.") 
+        _                           <- ("An exception was raised while running the action " <> tshow controller <> ".\n\n")
     let (RequestContext _ respond _ _ _) = ?requestContext
     let title = H.text (tshow exception)
     putStrLn (tshow exception)
