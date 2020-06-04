b0e5140eb3f55991d022ec0a270640313cf2c34c
laobiz <pothjonas@gmail.com>
2020-06-04 15:36:47 +0200

    further work

--- IHP/IDE/CodeGen/ControllerGenerator.hs
+++ IHP/IDE/CodeGen/ControllerGenerator.hs
@@ -236,42 +236,6 @@ qualifiedViewModuleName config viewName =
 pathToModuleName :: Text -> Text
 pathToModuleName moduleName = Text.replace "." "/" moduleName
 
-generateGenericView :: [Statement] -> ViewConfig -> [GeneratorAction]
-generateGenericView schema config = 
-        let 
-            controllerName = get #controllerName config
-            name = get #viewName config
-            singularName = config |> get #modelName
-            singularVariableName = lcfirst singularName
-            pluralVariableName = lcfirst controllerName
-            nameWithSuffix = name <> "View" --e.g. "TestView"
-
-            viewHeader =
-                ""
-                <> "module " <> qualifiedViewModuleName config name <> " where\n"
-                <> "import " <> get #applicationName config <> ".View.Prelude\n"
-                <> "\n"
-            
-            genericView = 
-                viewHeader
-                <> "data " <> nameWithSuffix <> " = " <> nameWithSuffix <> "\n"
-                <> "\n"
-                <> "instance View " <> nameWithSuffix <> " ViewContext where\n"
-                <> "    html " <> nameWithSuffix <> " { .. } = [hsx|\n"
-                <> "        <nav>\n"
-                <> "            <ol class=\"breadcrumb\">\n"
-                <> "                <li class=\"breadcrumb-item\"><a href={" <> indexAction <> "}>" <> Countable.pluralize name <> "</a></li>\n"
-                <> "                <li class=\"breadcrumb-item active\">" <> nameWithSuffix <> "</li>\n"
-                <> "            </ol>\n"
-                <> "        </nav>\n"
-                <> "        <h1>" <> nameWithSuffix <> "</h1>\n"
-                <> "    |]\n"
-        in
-            [ EnsureDirectory { directory = get #applicationName config <> "/View/" <> name }
-            , CreateFile { filePath = get #applicationName config <> "/View/" <> name <> "/" <> name <> ".hs", fileContent = genericView }
-            ]
-
-
 generateViews :: [Statement] -> ControllerConfig -> [GeneratorAction]
 generateViews schema config =
         let
